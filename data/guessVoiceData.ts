ae27e14780036966d2d72687d28c30849d4783cf
Danchik <a123@MacBook-Pro-123.local>
2025-12-22 15:31:09 +0300

    add some answers in voice

--- data/guessVoiceData.ts
+++ data/guessVoiceData.ts
@@ -19,37 +19,43 @@ export const guessVoiceData: any[] = [
     audioUrl: "/audio/voices/file1.mp3",
     originalAudioUrl: "/audio/voices/file1_original.mp4", // Добавьте оригинальный файл
     options: ["Таня", "Даша", "Юля", "Витя"],
-    correctAnswer: "Даша", // Или используйте correctAnswers: ["Даша", "Дашенька"]
+    correctAnswer: ["Даша", 'Дарья', 'Дашуля', 'Мазда 6', 'Дашка']
+    , // Или используйте correctAnswers: ["Даша", "Дашенька"]
   },
   {
     audioUrl: "/audio/voices/file3.mp3",
     originalAudioUrl: "/audio/voices/file3_original.mp3", // Добавьте оригинальный файл
     options: ["Маша", "Полина", "Петя", "Веня", "Дина", "Илья"],
-    correctAnswer: "Дина",
+    correctAnswer: ["Дина", 'Бобренок старший', "Динчик", "Диночка"]
+    ,
   },
   {
     audioUrl: "/audio/voices/file4.mp3",
     originalAudioUrl: "/audio/voices/file4_original.mp4", // Добавьте оригинальный файл
     options: ["Данил Я", "Витя", "Лена", "Яна", "Филипп"],
-    correctAnswer: "Данил Я",
+    correctAnswer: ["Данил", 'Денчик', "Яковлев", "Даниил", "Даня"]
+    ,
   },
   {
     audioUrl: "/audio/voices/file5.mp3",
     originalAudioUrl: "/audio/voices/file5_original.mp3", // Добавьте оригинальный файл
     options: ["Витя", "Данил Я", "Тима", "Аня", "Филипп"],
-    correctAnswer: ["Тима", 'Тимоха'],
+    correctAnswer: ["Тима", 'Тимофей', 'Черныш', 'Албанстрой', 'Тимоша', "Тимоха"]
+    ,
   },
   {
     audioUrl: "/audio/voices/file6.mp3",
     originalAudioUrl: "/audio/voices/file6_original.mp4", // Добавьте оригинальный файл
     options: ["Иван", "Мария", "Петя", "Маша", "Лиза Б", "Витя"],
-    correctAnswer: ["Витя", 'БадиВитька'],
+    correctAnswer: ["Виктор", 'Витя', "Б.д.к", "Викторио"]
+    ,
   },
   {
     audioUrl: "/audio/voices/file7.mp3",
     originalAudioUrl: "/audio/voices/file7_original.mp3", // Добавьте оригинальный файл
     options: ["Филипп", "Мария", "Маша", "Оля", "Лиза Б", "Данил Я"],
-    correctAnswer: "Филипп",
+    correctAnswer: ["Филипп", 'Филя', 'Филка', 'Филип', 'Фелип']
+    ,
   },
   {
     audioUrl: "/audio/voices/file8.mp3",
@@ -61,13 +67,15 @@ export const guessVoiceData: any[] = [
     audioUrl: "/audio/voices/file9.mp3",
     originalAudioUrl: "/audio/voices/file9_original.mp3", // Добавьте оригинальный файл
     options: ["Коля", "Филипп", "Руфа", "Данил Я", "Лиза Б"],
-    correctAnswer: "Лиза Б",
+    correctAnswer: ["Елизавета", 'не крутите мяч пж', 'Лизонька', 'Элиза', 'Лиз', 'Лиза']
+    ,
   },
   {
     audioUrl: "/audio/voices/file10.mp3",
     originalAudioUrl: "/audio/voices/file10_original.mp4", // Добавьте оригинальный файл
     options: ["Руфа", "Филипп", "Сашка", "Данил Я", "Лиза Б"],
-    correctAnswer: "Сашка",
+    correctAnswer: ["Александр 1-й", 'Саня', 'Владелец', 'Нина...', 'Санчес', "Саша", 'Александр', "Степанов"]
+    ,
   },
   // Добавьте больше вопросов здесь
 ];
